2218cc936a753ef8e01f7265a50967e61624b40d
Swapnali Bhujbal <swapnalibhujbal596@gmail.com>
2023-07-23 22:57:08 +0530

    Added popUpWindowHandleTest

---
+++ src/main/java/guru99/Scenario4PopupHandle.java
@@ -0,0 +1,33 @@
+package guru99;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Scenario4PopupHandle {
+	
+	WebDriver driver;
+	By addToCompareSonyXperia = By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[2]/div/div[3]/ul/li[2]/a");
+	By addToCompareIphone = By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[3]/div/div[3]/ul/li[2]/a");
+	By compareBtn = By.xpath("//button[@title='Compare']");
+	By closeSonyWindow = By.xpath("//button[@class='button']");
+	
+	public Scenario4PopupHandle(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void clickSonyXperia() {
+		driver.findElement(addToCompareSonyXperia).click();
+	}
+	
+	public void clickIphone() {
+		driver.findElement(addToCompareIphone).click();
+	}
+	public void clickComparetBtn() {
+		driver.findElement(compareBtn).click();
+	}
+	
+	public void clickCloseWindow() {
+		driver.findElement(closeSonyWindow).click();
+	}
+
+}
---
+++ src/test/java/guru99/Scenario4PopupHandleTest.java
@@ -0,0 +1,75 @@
+package guru99;
+
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Scenario4PopupHandleTest {
+
+	WebDriver driver;
+	String url ="http://live.techpanda.org/";
+	HomePage home;
+	MobilePage mobile;
+	Scenario4PopupHandle popUpHandle;
+
+	@BeforeMethod
+	public void setup() {
+
+		System.setProperty("webdriver.chrome.driver","E:\\Softwares\\chromedriver_win32 (2)\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+		driver.get(url);
+		home = new HomePage(driver);
+		mobile = new MobilePage(driver);
+		popUpHandle = new Scenario4PopupHandle(driver);
+
+	}
+
+
+	@Test
+	public void popUpHandle() throws InterruptedException {
+		//click mobile Menu
+		home.clickMobileMenu();
+		popUpHandle.clickSonyXperia();
+		popUpHandle.clickIphone();
+		popUpHandle.clickComparetBtn();
+		Thread.sleep(2000);
+		//close popup window
+		Set<String>handles = driver.getWindowHandles();
+		Iterator<String>it = handles.iterator();
+		//print size of window handle
+		System.out.println(handles.size());
+
+		String parentWindow = it.next();
+		System.out.println(parentWindow);
+
+		String childWindow = it.next();
+		System.out.println(childWindow);
+		//switch to childwindow
+		driver.switchTo().window(childWindow);
+		System.out.println(driver.getTitle());
+
+		popUpHandle.clickCloseWindow();
+		Thread.sleep(1000);
+		//switch to parent window
+		driver.switchTo().window(parentWindow);
+
+	}
+
+	@AfterMethod
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.close();
+	}
+
+
+}
